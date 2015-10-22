cdc3c0086993bb10ee0ee2f661348fac9ad2a541
Inggo <inggo.espinosa@gmail.com>
2015-10-22 20:13:01 +0800

    Add logo to nav

---
+++ wp-content/themes/theme-cura/nav.php
@@ -0,0 +1,11 @@
+<header class="container">
+	<div class="row">
+		<div class="col-lg-4 col-md-3 col-sm-2 col-xs-2">
+		    <div class="logo-container">
+				<img src="<?= get_header_image(); ?>" alt="<?php bloginfo('title') ?>">
+			</div>
+		</div>
+		<div class="col-lg-8 col-md-9 col-sm-10 col-xs-10">
+		</div>
+	</div>
+</header>
