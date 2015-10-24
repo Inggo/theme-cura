2c015057e91bca4e33ccbe9599b45f1e13bb9587
Inggo <inggo.espinosa@gmail.com>
2015-10-24 15:30:42 +0800

    Responsive menu & front-page header

--- wp-content/themes/theme-cura/nav.php
+++ wp-content/themes/theme-cura/nav.php
@@ -7,7 +7,7 @@
                 </div>
                 <nav class="nav-container navbar">
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-main" aria-expanded="false">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -15,7 +15,7 @@
                         </button>
                     </div>
 
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div class="collapse navbar-collapse" id="nav-main">
                         <ul class="nav navbar-nav">
                             <li><a href="#">Home</a></li>
                             <li><a href="#">Process</a></li>
